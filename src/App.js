55339cc12eb3a38819a3aa9942a79042bed8d35a
RodrigoReyes01 <71049819+RodrigoReyes01@users.noreply.github.com>
2022-05-18 22:21:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import NavBar from './Components/NavBar';
-import ProductList from './Components/ProductList';
+import { Component } from "react";
+import axios from "axios";
+import ProductCatalog from "./Components/ProductCatalog";
+import ShopNavbar from "./Components/ShopNavbar";
+import CategoryItems from "./Components/CategoryItems";
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import Cart from "./Components/Cart";
 
-function App() {
-  return (
-    <div>
-      <h1>Product Catalog</h1>
-      <ProductList random={true} cols="5" limit="30" />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cartProducts: [],
+      total: 0,
+      categories: [],
+      error: ''
+    }
+  }
+
+  fetch = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products/categories');
+      console.log(response.data)
+      this.setState({ categories: response.data })
+    }
+    catch (error) {
+      this.setState({ error: error })
+    }
+  };
+
+  componentDidMount() {
+    this.fetch();
+  };
+
+
+  handleAddToCart = (product) => {
+    const products = [...this.state.cartProducts];
+    products.push(product);
+    const price = product.price;
+    this.setState(
+      {
+        cartProducts: products,
+        total: this.state.total + price
+      }
+    )
+  }
+
+  handleDelete = (index) => {
+    const products = [...this.state.cartProducts];
+    const selected = products[index];
+    const price = selected.price;
+    products.splice(index, 1);
+    this.setState({ ...this.state, cartProducts: products, total: this.state.total - price });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <ShopNavbar categories={this.state.categories} products={this.state.cartProducts} total={this.state.total} handleDelete={this.handleDelete} />
+        <Routes>
+          <Route
+            path='/'
+            element={<ProductCatalog handleAddToCart={this.handleAddToCart} />}
+          />
+          <Route
+            path=':categoryName'
+            element={<CategoryItems />} />
+          <Route path='cart'
+            element={<Cart cartProducts={this.state.cartProducts} total={this.state.total} />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
